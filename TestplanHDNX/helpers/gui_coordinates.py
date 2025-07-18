9998549d29125fd38d73c5b85d45c690e106dc8f
OliwierUrbanski <oliwier.urbanski@strato.de>
2025-07-18 11:45:17 +0200

    feat(gui_coordinates): add GuiCoordinates class for GUI interaction coordinates

---
+++ TestplanHDNX/helpers/gui_coordinates.py
@@ -0,0 +1,8 @@
+# TestplanHDNX/helpers/gui_coordinates.py
+
+class GuiCoordinates:
+    """Zentrale Koordinaten für GUI-Interaktionen mit PyAutoGUI"""
+    MENU_ICON = (3073, 12)
+    USER_DROPDOWN = (2860, 86)
+    SETTINGS_ITEM = (2865, 289)
+    CLICK_PAUSE = 0.3
