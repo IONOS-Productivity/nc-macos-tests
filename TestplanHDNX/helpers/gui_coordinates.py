76ad932c52cdb5e14e9a58bb5fd8fd24df7d7c2e
OliwierUrbanski <oliwier.urbanski@strato.de>
2025-07-30 14:02:24 +0200

    fix(gui_coordinates): update CLICK_PAUSE value and add LOGOUT_ITEM and USER_SELF_ITEM coordinates

--- TestplanHDNX/helpers/gui_coordinates.py
+++ TestplanHDNX/helpers/gui_coordinates.py
@@ -5,4 +5,6 @@ class GuiCoordinates:
     MENU_ICON = (3073, 12)
     USER_DROPDOWN = (2860, 86)
     SETTINGS_ITEM = (2865, 289)
-    CLICK_PAUSE = 0.3
+    CLICK_PAUSE = 3.3
+    LOGOUT_ITEM = (3169, 204)
+    USER_SELF_ITEM = (3076, 175)
